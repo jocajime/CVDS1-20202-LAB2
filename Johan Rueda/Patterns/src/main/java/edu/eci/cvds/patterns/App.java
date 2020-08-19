e8ef65cd74abd021068e0266e086a42293921b2f
johanrueda <johan.rueda@mail.escuelaing.edu.co>
2020-08-19 00:00:44 -0500

    Modificaciones

--- Johan Rueda/Patterns/src/main/java/edu/eci/cvds/patterns/App.java
+++ Johan Rueda/Patterns/src/main/java/edu/eci/cvds/patterns/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
      {
+	System.out.println("Hello");
 	for (int i=0; i<args.length;i++){
 		if (i==args.length-1){
 			System.out.print(args[i]+"!");
